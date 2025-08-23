6f7297b97afdfc757232377d9078e04ffea9b840
鲸鱼在游泳 <perchecc@163.com>
2025-08-24 02:19:16 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,10 +18,15 @@ import {
 } from '@shikijs/transformers'
 import tailwindcss from '@tailwindcss/vite'
 
+import { defineConfig, passthroughImageService } from 'astro/config';
+
 // https://astro.build/config
 export default defineConfig({
   site,
   prefetch: { prefetchAll: true },
+  image: {
+    service: passthroughImageService(),
+  },
   markdown: {
     remarkRehype: { footnoteLabel: ' ' },
     shikiConfig: {
